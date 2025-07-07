d275d056bf1c3941837b0e00e9d48391cb8b48dc
DMB <ddimocmb@gmail.com>
2025-07-08 00:18:02 +0500

    added an example of working with accept window

---
+++ project/3_actions/test_dialog_accept.py
@@ -0,0 +1,14 @@
+import pytest
+
+from playwright.sync_api import Playwright, sync_playwright, expect, Page
+
+def test_dialogs_without_processing(page: Page):
+    page.goto("https://zimaev.github.io/dialog/")
+    page.get_by_text("Диалог Alert").click()
+    page.get_by_text("Диалог Confirmation").click()
+    page.get_by_text("Диалог Prompt").click()
+
+def test_dialogs(page: Page):
+    page.goto("https://zimaev.github.io/dialog/")
+    page.on("dialog", lambda dialog: dialog.accept())
+    page.get_by_text("Диалог Confirmation").click()
