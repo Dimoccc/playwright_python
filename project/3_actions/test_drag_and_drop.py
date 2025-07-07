556b1239257a3d64a6f19419ff7dd4c1d918fc94
DMB <ddimocmb@gmail.com>
2025-07-08 00:17:31 +0500

    added an example of working with a drag and drop

---
+++ project/3_actions/test_drag_and_drop.py
@@ -0,0 +1,7 @@
+import pytest
+
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+def test_drag_and_drop(page):
+    page.goto('https://zimaev.github.io/draganddrop/')
+    page.drag_and_drop("#drag", "#drop")
