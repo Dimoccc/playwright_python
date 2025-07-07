26241b4a4b1e37a7ed8ae5b78959cad9891fb8f3
DMB <ddimocmb@gmail.com>
2025-07-08 00:15:37 +0500

    added an example of working with checkbox

---
+++ project/3_actions/test_checkbox.py
@@ -0,0 +1,19 @@
+import pytest
+
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+def test_checkbox_locator(page):
+    page.goto('https://zimaev.github.io/checks-radios/')
+    page.locator("text=Default checkbox").check()
+    page.locator("text=Checked checkbox").check()
+    page.locator("text=Default radio").check()
+    page.locator("text=Default checked radio").check()
+    page.locator("text=Checked switch checkbox input").check()
+
+def test_checkbox_click(page):
+    page.goto('https://zimaev.github.io/checks-radios/')
+    page.locator("text=Default checkbox").click()
+    page.locator("text=Checked checkbox").click()
+    page.locator("text=Default radio").click()
+    page.locator("text=Default checked radio").click()
+    page.locator("text=Checked switch checkbox input").click()
