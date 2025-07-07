6cc19659444a975dbef34a966f7db4ca15a8dd1e
DMB <ddimocmb@gmail.com>
2025-07-08 00:16:43 +0500

    added an example of working with drop-down list

---
+++ project/3_actions/test_select_option.py
@@ -0,0 +1,13 @@
+import pytest
+
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+def test_select(page):
+    page.goto('https://zimaev.github.io/select/')
+    page.select_option('#floatingSelect', value="3")
+    page.select_option('#floatingSelect', index=1)
+    page.select_option('#floatingSelect', label="Нашел и завел bug")
+
+def test_select_multiple(page):
+    page.goto('https://zimaev.github.io/select/')
+    page.select_option('#skills', value=["playwright", "python"])
