76dd9953fd6c512d3ea64da5ab3ff262aaa7b5d8
DMB <ddimocmb@gmail.com>
2025-07-18 23:28:59 +0500

    modified:   .gitignore new file:   project/4_2_mocking/test_moking.py new file:   project/4_advanced_features/debug_note.txt

---
+++ project/4_2_mocking/test_moking.py
@@ -0,0 +1,7 @@
+import pytest
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+def test_listen_network(page):
+    page.on("request", lambda request: print(">>", request.method, request.url))
+    page.on("response", lambda response: print("<<", response.status, response.url))
+    page.goto('https://osinit.ru/')
