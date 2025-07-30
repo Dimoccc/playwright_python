a486039b599f2cd2e0b7f0f17ec42552a8e0b8f6
DMB <ddimocmb@gmail.com>
2025-07-31 00:38:31 +0500

    modified:   project/4_3_api/test_api.py new file:   project_POM/pages/__init__.py new file:   project_POM/pages/login_page.py new file:   project_POM/requarements.txt new file:   project_POM/tests/__init__.py new file:   project_POM/tests/test_login.py modified:   pytest.ini

--- project/4_3_api/test_api.py
+++ project/4_3_api/test_api.py
@@ -5,3 +5,27 @@ def test_inventory(page):
     response = page.request.get('https://petstore.swagger.io/v2/store/inventory')
     print(response.status)
     print(response.json())
+
+def test_add_user(page):
+    data = [
+              {
+                "id": 9743,
+                "username": "fsd",
+                "firstName": "fff",
+                "lastName": "ggg",
+                "email": "bbb",
+                "password": "tt",
+                "phone": "333",
+                "userStatus": 0
+              }
+            ]
+    header = {
+        'accept': 'application/json',
+        'content-Type': 'application/json'
+    }
+    response = page.request.post('https://petstore.swagger.io/v2/user/createWithArray',data=data, headers=header)
+    print('status = ',response.status)
+    print('json = ',response.json())
+    print('text = ', response.text())
+    print('json = ', response.json())
+    print('body = ', response.body())
